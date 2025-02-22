c5c6e90900b012c7472eb40e3dd8456ab25d6960
Brojopriya <brojopriyanag@gmail.com>
2025-02-22 14:24:40 +0600

    comment modify

--- post/models.py
+++ post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -6,5 +7,8 @@ class Post(models.Model):
     content = models.TextField()
     publish_date= models.DateField()
 
+
 class comment(models.Model):
     text = models.CharField(max_length=100)
+    post= models.ForeignKey(Post,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
