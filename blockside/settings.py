6ab1c3e87a4cc2f10e3b20a20262033310a3014f
Brojopriya <brojopriyanag@gmail.com>
2025-02-27 16:26:30 +0600

    post_list update

--- blockside/settings.py
+++ blockside/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR,"staticfiles")
+]
 STATIC_URL = 'static/'
 
 # Default primary key field type
